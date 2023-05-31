c8a0a4d50f053021bc67172401b894f73946289b
Stefano <stefanop21@outlook.es>
2023-05-31 00:21:06 -0500

    EmpleadoMan02

---
+++ ProyBanco_GUI/EmpleadoGUI/EmpleadoMan02.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Otros...
+using ProyBanco_BE;
+using ProyBanco_BL;
+
+namespace ProyBanco_GUI.EmpleadoGUI
+{
+    public partial class EmpleadoMan02 : Form
+    {
+        EmpleadoBE objEmpleadoBE = new EmpleadoBE();
+        EmpleadoBL objEmpleadoBL = new EmpleadoBL();
+
+        public EmpleadoMan02()
+        {
+            InitializeComponent();
+        }
+
+        private void EmpleadoMan02_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                CargarUbigeo("14", "01", "01");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void CargarUbigeo(String IdDepa, String IdProv, String IdDist)
+        {
+            UbigeoBL objUbigeoBL = new UbigeoBL();
+
+            cboDepartamento.DataSource = objUbigeoBL.Ubigeo_Departamentos();
+            cboDepartamento.ValueMember = "ID_Departamento";
+            cboDepartamento.DisplayMember = "Departamento";
+            cboDepartamento.SelectedValue = IdDepa;
+
+            cboProvincia.DataSource = objUbigeoBL.Ubigeo_ProvinciasDepartamento(IdDepa);
+            cboProvincia.ValueMember = "ID_Provincia";
+            cboProvincia.DisplayMember = "Provincia";
+            cboProvincia.SelectedValue = IdProv;
+
+            cboDistrito.DataSource = objUbigeoBL.Ubigeo_DistritosProvinciaDepartamento(IdDepa, IdProv);
+            cboDistrito.ValueMember = "ID_Distrito";
+            cboDistrito.DisplayMember = "Distrito";
+            cboDistrito.SelectedValue = IdDist;
+        }
+
+        // Función general para detectar números
+        private void DetectarNumeros(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 8)
+            {
+                if (char.IsDigit(e.KeyChar) == false)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void DetectarDocumento(object sender, EventArgs e)
+        {
+            if (optDNI.Checked)
+            {
+                txtDocumento.MaxLength = 8;
+            }
+            else
+            {
+                txtDocumento.MaxLength = 12;
+            }
+        }
+
+        // Función para verificar el patrón de correo
+        private bool VerificarCorreo(String strCorreo)
+        {
+            // Patrón de expresión regular para validar el formato del correo electrónico
+            string patron = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+
+            // Verificar si el correo electrónico coincide con el patrón
+            bool esValido = Regex.IsMatch(strCorreo, patron);
+
+            return esValido;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validaciones
+                if (txtNombre.Text.Trim() == String.Empty)
+                {
+                    throw new Exception("El nombre es olbigatorio.");
+                }
+
+                if (txtApellidoP.Text.Trim() == String.Empty)
+                {
+                    throw new Exception("El apellido paterno es obligatorio.");
+                }
+
+                if (txtApellidoM.Text.Trim() == String.Empty)
+                {
+                    throw new Exception("El apellido materno es obligatorio.");
+                }
+
+                if (txtTelefono.Text.Trim() == String.Empty)
+                {
+                    throw new Exception("El número de telefono es obligatorio.");
+                }
+
+                if (txtTelefono.Text.Trim().Length < 9)
+                {
+                    throw new Exception("El número de telefono debe tener 9 digitos.");
+                }
+
+                if (txtCorreo.Text.Trim() == String.Empty)
+                {
+                    throw new Exception("La dirección de correo es obligatoria.");
+                }
+
+                if (!VerificarCorreo(txtCorreo.Text.Trim()))
+                {
+                    throw new Exception("La dirección de correo es errónea.");
+                }
+
+                if (txtDocumento.Text.Trim() == String.Empty)
+                {
+                    throw new Exception("El número de documento es obligatorio.");
+                }
+
+                if (txtDocumento.Text.Trim().Length < 8 || txtDocumento.Text.Trim().Length > 8)
+                {
+                    throw new Exception("El número de DNI debe tener 8 dígitos.");
+                }
+
+                if ((optCarnet.Checked || optPasaporte.Checked) && txtDocumento.Text.Length < 12)
+                {
+                    throw new Exception("El Carnet de Extranjería o Pasaporte debe tener 12 dígitos.");
+                }
+
+                // Definición
+                Int16 tipoDocumento;
+                if (optDNI.Checked)
+                {
+                    tipoDocumento = 1;
+                }
+                else if (optCarnet.Checked)
+                {
+                    tipoDocumento = 2;
+                }
+                else
+                {
+                    tipoDocumento = 3;
+                }
+
+                // Insertamos
+                objEmpleadoBE.Nom_Emp = txtNombre.Text.Trim();
+                objEmpleadoBE.Ape_pat_Emp = txtApellidoP.Text.Trim();
+                objEmpleadoBE.Ape_mat_Emp = txtApellidoM.Text.Trim();
+                objEmpleadoBE.Tel_Emp = txtTelefono.Text.Trim();
+                objEmpleadoBE.Cor_Emp = txtCorreo.Text.Trim();
+                objEmpleadoBE.Tip_doc_Emp = tipoDocumento;
+                objEmpleadoBE.Num_doc_Emp = txtDocumento.Text.Trim();
+                objEmpleadoBE.Img_Emp = 1; // todo: Por resolver
+                objEmpleadoBE.Id_Ubigeo = cboDepartamento.SelectedValue.ToString() + cboProvincia.SelectedValue.ToString() + cboDistrito.SelectedValue.ToString();
+                objEmpleadoBE.Est_Emp = Convert.ToInt16(chkActivo.Checked);
+
+                objEmpleadoBE.Usu_Registro = clsCredenciales.Usuario;
+
+                // Enviamos los datos
+                if (objEmpleadoBL.InsertarEmpleado(objEmpleadoBE))
+                {
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo guardar el registro.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void cboDepartamento_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            CargarUbigeo(cboDepartamento.SelectedValue.ToString(), "01", "01");
+        }
+
+        private void cboProvincia_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            CargarUbigeo(cboDepartamento.SelectedValue.ToString(), cboProvincia.SelectedValue.ToString(), "01");
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
